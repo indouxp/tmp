b96bd75079b23db7f7b7a4359c3ea6728035cf21
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2013-07-16 18:43:27 +0900

    update teiden.sh

--- teiden.sh
+++ teiden.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+LF=$(printf '\\\012_');LF=${LF%_}
 curl -s http://teideninfo.tepco.co.jp/html/00000000000.html |
   # utf-8とし、停電情報のテーブルを抜き出す
   nkf -w                                                    |
@@ -8,21 +9,53 @@ curl -s http://teideninfo.tepco.co.jp/html/00000000000.html |
   grep -v "tbody>"                                          |
   # trタブを改行に変換
   tr -d '\012'                                              |
-  sed '$a
+  sed '$a\
 '                                                           |
-  sed "s/<tr>/\n/g"                                         |
+  sed "s/<tr>/${LF}/g"                                      |
   sed "s/<\/tr>//g"                                         |
   # <td.*>を取る
   sed "s/<td[^>][^>]*>//g"                                  |
   sed "s/<\/td>//g"                                         |
   #
   tee /tmp/href.txt                                         |
-  sed "s/<a[^>][^>]*>//"
+  sed "s/<a[^>][^>]*>//"                                    |
+  awk '
+      {
+        if (NF > 0) {
+          if ($1 != "&nbsp;" && $2 != "&nbsp;") {
+            printf("%s %s\n", $1, $2);
+          }
+        }
+      }'
 
 for url in `cat /tmp/href.txt|sed 's/<a href="//'|sed 's/">.*//'`
 do
   curl -s http://teideninfo.tepco.co.jp/html/${url}         |
   # utf-8とし、停電情報のテーブルを抜き出す
   nkf -w                                                    |
-  tee log.txt
+  sed -n '/<table.*"停電情報">/,/<\/table>/p'               |
+  # 都道府県と停電情報のみを抜き出す
+  sed -n "/<tbody>/,/<\/tbody>/p"                           |
+  grep -v "tbody>"                                          |
+  # trタブを改行に変換
+  tr -d '\012'                                              |
+  sed '$a\
+'                                                           |
+  sed "s/<tr>/${LF}/g"                                      |
+  sed "s/<\/tr>//g"                                         |
+  # <td.*>を取る
+  sed "s/<td[^>][^>]*>//g"                                  |
+  sed "s/<a.*\/a>//"                                        |
+  sed "s/<\/td>//g"                                         | tee log.txt |
+  awk '
+      {
+        if (NF > 0) {
+          if ($1 != "&nbsp;" && $2 != "&nbsp;") {
+            printf("%s %s\n", $1, $2);
+          }
+          if ($3 != "&nbsp;" && $4 != "&nbsp;") {
+            printf("%s %s\n", $3, $4);
+          }
+        }
+      }'
 done
