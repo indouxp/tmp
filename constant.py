fbc46cab622d1f2c6a11f0282c768c2a2ed078c1
Tatsuo Indou <indou.tsystem@gmail.com>
2020-08-13 19:13:39 +0900

    Create constant.py

---
+++ constant.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# 処理概要: 共通定数定義
+#
+
+# 送信メールデータCSVのフィールド定義
+# 結果管理ファイルは、送信日時(SEND_YMDHMS)が加わる
+# エラーリストファイルは、エラー情報(ERR_YMDHMS,ERR_CODE,ERR_MSG)が加わる
+
+MYPAGE_ID = 0
+MAIL_TRANSMIT_SEQUENCE = 1
+MAIL_ADDRESS = 2
+MAIL_TITLE = 3
+MAIL_MAINTEXT = 4
+SEND_YMDHMS = 5
+ERR_YMDHMS = 5
+ERR_CODE = 6
+ERR_MSG = 7
